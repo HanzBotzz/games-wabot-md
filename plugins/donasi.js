b689c8714da2203b6d82b01fdd07d0e6617df526
HanzBotzz <109465690+HanzBotzz@users.noreply.github.com>
2022-07-18 20:32:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • Indosat Ooredoo [085779150501]
+│ • Tri [089512630591]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • https://youtube.com/channel/UCs8sFcihqsye35hnom6CEXQ
+│ • https://vt.tiktok.com/ZSdyjBq77/
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
